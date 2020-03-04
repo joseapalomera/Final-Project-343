f6b7d0133bcb6ea3f2f02d7099eaa5cff3847ef1
Gurjepaul <47045111+PaulBrarMallCop@users.noreply.github.com>
2020-03-03 23:05:33 -0800

    Add files via upload

--- Program4/Program4/Movie/Movie.cpp
+++ Program4/Program4/Movie/Movie.cpp
@@ -2,8 +2,114 @@
 //  Movie.cpp
 //  Program4
 //
-//  Created by Jose Palomera on 3/3/20.
-//  Copyright © 2020 Jose Palomera. All rights reserved.
+//  Created by Gurjepaul Brar on 3/3/20.
+//  Copyright © 2020 Gurjepaul Brar. All rights reserved.
 //
 
 #include <stdio.h>
+#include "Movie.h"
+
+Movie::Movie()
+{
+    this->movieType = 0;
+    this->stock = -1;
+    this->director = "";
+    this->title = "";
+    this->releaseYear = -1;
+}
+
+Movie::Movie(char movieType, int stock, string director, string title, int releaseYear)
+{
+    this->movieType = movieType;
+    this->stock = stock;
+    this->director = director;
+    this->title = title;
+    this->releaseYear = releaseYear;
+}
+
+Movie::~Movie(){}
+
+void Movie::setMovieType(char movieType)
+{
+    this->movieType = movieType;
+}
+
+void Movie::setStock(int stock)
+{
+    this->stock = stock;
+}
+
+void Movie::setDirector(string director)
+{
+    this->director = director;
+}
+
+void Movie::setTitle(string title)
+{
+    this->title = title;
+}
+
+void Movie::setReleaseYear(int releaseYear)
+{
+    this->releaseYear = releaseYear;
+}
+
+char Movie::getMovieType() const
+{
+    return movieType;
+}
+
+int Movie::getStock() const
+{
+    return stock;
+}
+
+string Movie::getDirector() const
+{
+    return director;
+}
+
+string Movie::getTitle() const
+{
+    return title;
+}
+
+int Movie::getReleaseYear( )const
+{
+    return releaseYear;
+}
+
+bool Movie::operator==(const Movie& m) const
+{
+    if ((movieType == m.movieType) && (stock == m.stock) && (director == m.director) && (title == m.title) && (releaseYear == m.releaseYear))
+    {
+        return true;
+    }
+    
+    return false;
+}
+
+bool Movie::operator!=(const Movie& m)const
+{
+    return !(*this == m);
+}
+
+bool Movie::operator>(const Movie& m) const // need to complete
+{
+    return false;
+}
+
+bool Movie::operator<(const Movie& m) const // need to complete
+{
+    return false;
+}
+
+ostream& operator<<(ostream &os, const Movie &other)
+{
+    os << other.getMovieType() << ", ";
+    os << other.getStock() << ", ";
+    os << other.getDirector() << ", ";
+    os << other.getTitle() << ", ";
+    os << other.getReleaseYear() << endl;
+    return os;
+}
--- Program4/Program4/Movie/Movie.h
+++ Program4/Program4/Movie/Movie.h
@@ -2,12 +2,53 @@
 //  Movie.h
 //  Program4
 //
-//  Created by Jose Palomera on 3/3/20.
-//  Copyright © 2020 Jose Palomera. All rights reserved.
+//  Created by Gurjepaul Brar on 3/3/20.
+//  Copyright © 2020 Gurjepaul Brar. All rights reserved.
 //
 
 #ifndef Movie_h
 #define Movie_h
 
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Movie
+{
+    friend ostream& operator<<(ostream &os, const Movie &other);
+
+public:
+    // Constructors
+    Movie();
+    Movie(char movieType, int stock, string director, string title, int releaseYear);
+    ~Movie();
+    
+    // Setters
+    void setMovieType(char movieType);
+    void setStock(int stock);
+    void setDirector(string director);
+    void setTitle(string title);
+    void setReleaseYear(int releaseYear);
+    
+    // Getters
+    char getMovieType() const;
+    int getStock() const;
+    string getDirector() const;
+    string getTitle() const;
+    int getReleaseYear( )const;
+    
+    // Operator overload
+    bool operator==(const Movie& m) const;
+    bool operator!=(const Movie& m)const;
+    bool operator>(const Movie& m) const;
+    bool operator<(const Movie& m) const;
+
+private:
+    char movieType;
+    int stock;
+    string director;
+    string title;
+    int releaseYear;
+};
 
 #endif /* Movie_h */
